00dd52d8261cf6c28054290f54e1bf254e618900
Martyna Machowiak <martyna.machowiak@gmail.com>
2021-11-13 23:57:45 +0000

    Add getImages() request

---
+++ src/request/getImages.js
@@ -0,0 +1,19 @@
+// import axios from "axios";
+
+import axios from "axios";
+
+export const getImages = (query, setImages) => {
+  axios
+    .get(`https://images-api.nasa.gov/search?&media_type=image&q=${query}`)
+    .then((response) => {
+      const images = response.data.collection.items.map((imageSource) => {
+        return {
+          id: imageSource.data[0].nasa_id,
+          url: imageSource.links[0].href,
+          description: imageSource.data[0].description,
+          title: imageSource.data[0].title,
+        };
+      });
+      setImages(images);
+    });
+};
