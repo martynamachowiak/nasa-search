a73fd24625e036da758b274dd319484ab4ac22fa
Martyna Machowiak <martyna.machowiak@gmail.com>
2021-11-14 18:31:01 +0000

    Add search criteria checkboxes

--- src/components/SearchTool/SearchTool.js
+++ src/components/SearchTool/SearchTool.js
@@ -1,16 +1,56 @@
 import React, { useState } from "react";
 
 const SearchTool = ({ onSubmit }) => {
-  const [value, setValue] = useState("");
+  const [fields, setFields] = useState({
+    query: null,
+    searchCriteria: [],
+  });
+
+  const updateQuery = (e) => {
+    setFields({
+      ...fields,
+      query: e.target.value,
+    });
+  };
+
+  const updateSearchCriteria = (e) => {
+    const { name } = e.target;
+
+    const searchCriteria = fields.searchCriteria.includes(name)
+      ? fields.searchCriteria.filter((el) => el !== name)
+      : [...fields.searchCriteria, name];
+
+    setFields({
+      ...fields,
+      searchCriteria,
+    });
+  };
 
   return (
     <div>
       <input
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        placeholder="e.g. moon"
+        type="text"
+        value={fields.query}
+        onChange={(e) => updateQuery(e)}
+        placeholder="e.g. space cat"
       />
-      <button onClick={() => onSubmit(value)}>Search</button>
+      <button onClick={() => onSubmit(fields.query)}>Search</button>
+      <label>
+        <input
+          name="images"
+          type="checkbox"
+          onChange={(e) => updateSearchCriteria(e)}
+        />
+        Images
+      </label>
+      <label>
+        <input
+          name="video"
+          type="checkbox"
+          onChange={(e) => updateSearchCriteria(e)}
+        />
+        Videos
+      </label>
     </div>
   );
 };
